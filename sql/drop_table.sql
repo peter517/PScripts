22f4f76a353c306ecf3e4dab092b3a8fab624e4f
PenGuin <peter517@126.com>
2013-09-16 20:17:04 +0800

    sql

---
+++ sql/drop_table.sql
@@ -0,0 +1,12 @@
+drop sequence tb_code_sequence;
+
+drop table sys_organ;
+drop table sys_employee;
+drop table employee_right;
+drop table employee_operate_info;
+drop table sys_data_collector;
+drop table wellworker_well;
+drop table collect_data_01;
+drop table checkin_info;
+drop table checkin_task;
+drop table illegal_data;
---
+++ sql/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO sys_employee
+   (EMPLOYEE_ID, EMPLOYEE_TYPE, NAME, PARENT_ORGAN_ID, PASSWORD)
+VALUES
+   ('000001', '管理员', 'admin', '00', 'e10adc3949ba59abbe56e057f20f883e');
+
+INSERT INTO sys_employee
+   (EMPLOYEE_ID, EMPLOYEE_TYPE, NAME, PARENT_ORGAN_ID, PASSWORD)
+VALUES
+   ('000003', '普通员工', 'weishunlian', '00', 'e10adc3949ba59abbe56e057f20f883e');
+
+INSERT INTO sys_employee
+   (EMPLOYEE_ID, EMPLOYEE_TYPE, NAME, PARENT_ORGAN_ID, PASSWORD)
+VALUES
+   ('000004', '普通员工', 'weixuelian', '00', 'e10adc3949ba59abbe56e057f20f883e');
+
+INSERT INTO sys_employee
+   (EMPLOYEE_ID, EMPLOYEE_TYPE, NAME, PARENT_ORGAN_ID, PASSWORD)
+VALUES
+   ('000005', '普通员工', 'lushasha', '00', 'e10adc3949ba59abbe56e057f20f883e');
