d14079f4e05e4c73d22a40be86a660cc3fa095da
pengjun <pengjun@pengjun.(none)>
2013-09-18 12:39:43 +0800

    git

--- shell/add_sys_cmd.sh
+++ shell/add_sys_cmd.sh
@@ -10,11 +10,19 @@ cmd=$1
 script=$2
 
 cd ~
+isExist=`grep -w "alias ""$1" $envfile`
+
+if [ "$isExist" ];then
+	cd -
+	echo "cmd existed!"
+	return 1;
+fi
 
 insertNum=`grep -nw "alias" $envfile | cut -d : -f 1 | tail -1`
  
 sed -i $insertNum'a''alias '$cmd'='\"$script\" $envfile 
 
 source .bashrc
+cd -
 echo "add success"
 
---
+++ shell/git_commit.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+if [ -z "$1" ];then
+        echo "no parameter"
+        return 1;
+fi
+
+git add -A && git commit -m $1 && git push
+
+
