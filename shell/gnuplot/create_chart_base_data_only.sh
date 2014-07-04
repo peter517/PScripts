1e9f0cb79b2135bb80818501ca4fc5d1ce2560e8
xiaocheng <xiaocheng@mmpc.(none)>
2014-07-04 15:26:27 +0800

    add maven moudle

--- shell/gnuplot/create_chart_base_data_only.sh
+++ shell/gnuplot/create_chart_base_data_only.sh
@@ -10,7 +10,6 @@
 
 data_file=$1
 yfield="KB"
-xfield="Time"
 title="Videochat Memory Tendency"
 create_date=`date +%Y-%m-%d-%H-%M-%S`
 output_chart_file="vc-$create_date.png"
@@ -21,10 +20,7 @@ set terminal pngcairo lw 2
 set terminal png size 1280, 720
 set title \"$title\"
 set ylabel \"$yfield\"
-set xlabel \"$xfield\"
-set xdata time
-set timefmt \"$time_format\"
 set grid
 set output './$output_chart_file'
-plot \"$data_file\" using 1:2 with linespoints linecolor 3 linewidth 2 pointtype 7 pointsize 1
+plot \"$data_file\" using 1 with linespoints linecolor 3 linewidth 2 pointtype 7 pointsize 1
 " | gnuplot
--- tmp/modify_head_commnet.sh
+++
@@ -1,8 +0,0 @@
-
-FILE_NAME=$1
-CREATE_TIME=`date +%Y-%m-%d_%H:%M:%S`
-SRCRIPT_HEADER="#!/bin/bash\n#Copyright (C) 2010-2020 PScript Project\n#Author:pengjun\n#Email:peter517@126.com\n#CreateTime:${CREATE_TIME}\n#Comments:\n#"
-
-sed -i "1 a$SRCRIPT_HEADER" $FILE_NAME
-sed -i "1d" $FILE_NAME
-
