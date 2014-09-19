641d4fd4c81dd55db73d2a16105e8d65d9ce01a6
xiaocheng <xiaocheng@mmpc>
2014-09-19 13:24:08 +0800

    modify python server

--- python/server_webpy.py
+++ python/server_webpy.py
@@ -1,6 +1,10 @@
 
 import web,os
 import subprocess
+import commands
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')  
 
 render = web.template.render('templates/')
 
@@ -11,9 +15,10 @@ urls = (
 class index:
     	def __init__(self):
     		self.registerForm = web.form.Form(
-            	web.form.Textarea('crash_log', web.form.notnull,rows = 40, cols=100,  description='put crash log to left area'),
+            	web.form.Textarea('crash_log', web.form.notnull,rows = 30, cols=150,  description='submit crash log to left area'),
             	web.form.Textbox('so_build_date', description='mmpc so build date'),
             	web.form.Button('create stack'),
+            	#validators = [web.form.Validator("submit crash log too long", lambda i: len(i.crash_log) < 10000 )]
         )
 
     	def GET(self):
@@ -26,9 +31,14 @@ class index:
             		return render.form(form)
         	else:
 			crash_log = form.d.crash_log
+
+			crash_log_file="crash.log"
+			f = open(crash_log_file, "w")
+			f.write(crash_log)
+
 			so_build_date = form.d.so_build_date
 			symbol_path=( "/var/www/xtrunk/symbol/" + so_build_date + "/")
-			crash_stack = os.popen("echo '%s' | ndk-stack -sym %s | head -n 30" % (crash_log, symbol_path) ).read()
+			crash_stack = os.popen("cat '%s' | ndk-stack -sym %s | head -n 30" % (crash_log_file, symbol_path) ).read()
 
 			session_so_path=(symbol_path + "libsessionvc.so")
 			so_version = os.popen("strings %s | grep mmpc_version " % (session_so_path)).read()
