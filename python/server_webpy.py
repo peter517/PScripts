5d82295a803d93c3f8df4e826cc0f73d726dc510
xiaocheng <xiaocheng@mmpc>
2014-09-19 10:23:22 +0800

    modify python server

--- python/server_webpy.py
+++ python/server_webpy.py
@@ -11,7 +11,8 @@ urls = (
 class index:
     	def __init__(self):
     		self.registerForm = web.form.Form(
-            	web.form.Textarea('crash_log', web.form.notnull,rows = 40, cols=100,  description='put crash log to this area'),
+            	web.form.Textarea('crash_log', web.form.notnull,rows = 40, cols=100,  description='put crash log to left area'),
+            	web.form.Textbox('so_build_date', description='mmpc so build date'),
             	web.form.Button('create stack'),
         )
 
@@ -25,10 +26,11 @@ class index:
             		return render.form(form)
         	else:
 			crash_log = form.d.crash_log
-			symbol_path="/var/www/xtrunk/symbol"
+			so_build_date = form.d.so_build_date
+			symbol_path=( "/var/www/xtrunk/symbol/" + so_build_date + "/")
 			crash_stack = os.popen("echo '%s' | ndk-stack -sym %s | head -n 30" % (crash_log, symbol_path) ).read()
 
-			session_so_path=("/var/www/xtrunk/symbol/" + "libsessionvc.so")
+			session_so_path=(symbol_path + "libsessionvc.so")
 			so_version = os.popen("strings %s | grep mmpc_version " % (session_so_path)).read()
 
             		return "\nServer %s \n\n %s" % (so_version, crash_stack)
